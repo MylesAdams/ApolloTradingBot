1adf9950ed3d0d12248ed30239ebcce309debd14
AndrewLaux <laux.m.andrew@gmail.com>
2018-01-24 01:41:15 -0800

    Declare LogIn class, carries user credentials

---
+++ src/log_in.h
@@ -0,0 +1,24 @@
+//file: log_in.h
+//written by ANDREW LAUX
+
+#ifndef log_in_h
+#define log_in_h
+#include<string>
+
+//Class: LogIn
+class LogIn {
+private:
+	std::string user;
+	std::string pass;
+	std::string key;
+public:
+	LogIn() :user(""), pass(""), key("") {};
+	void setLogIn(std::string user, std::string pass, std::string key);
+	void clear();
+	bool isComplete();
+	std::string getUser();
+	std::string getPass();
+	std::string getKey();
+};
+
+#endif // !log_in.h
