b61b8a5dbada23dd92b34a00d7e7411b1ce2da15
AndrewLaux <laux.m.andrew@gmail.com>
2018-01-24 01:40:03 -0800

    A test driver for gui back end code

---
+++ src/gui_test_driver.cpp
@@ -0,0 +1,19 @@
+//file: gui_test_driver.cpp
+//written by ANDREW LAUX
+
+#include"exchange_accnt.h"
+#include"log_in.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+//Test
+int main() {
+	
+
+	//ExitProgram
+	cout << "Test complete . . .";
+	cin.get();
+	return 0;
+}
