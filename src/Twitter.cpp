56d44ac15ece3b495668aef9a9a8a496beafb02b
GavinFrazar <gavinfrazar@gmail.com>
2018-02-20 12:10:11 -0800

    url constructed for twitter

--- src/Twitter.cpp
+++ src/Twitter.cpp
@@ -1,6 +1,6 @@
 #include "Twitter.h"
 
-utility::string_t Apollo::Bot::Twitter::percentEncode(const utility::string_t& str)
+utility::string_t Apollo::Bot::Twitter::percentEncode(const utility::string_t& s)
 {
     std::string str = utility::conversions::utf16_to_utf8(s);
     std::vector<unsigned char> buffer;
@@ -34,7 +34,8 @@ utility::string_t Apollo::Bot::Twitter::percentEncode(const utility::string_t& s
 
     std::string encoded_str(buffer.begin(), buffer.end());
     std::cout << encoded_str;
-    return encoded_str;
+    utf16string t = U("asdf");
+    return t;
 }
 
 void Apollo::Bot::Twitter::saveSettings()
@@ -74,41 +75,49 @@ std::stringstream Apollo::Bot::Twitter::requestResponse(const std::string & reso
     // Get time since UNIX epoch
     uint64_t utc = datetime::utc_timestamp();
     
-    string_t method = U("GET");
+    utf8string method = "GET";
     string_t content_type = U("application/json");
-    string_t consumer_key = utility::conversions::to_string_t(this->consumer_key_);
+    //encode===========
+    string_t oauth_consumer_key = utility::conversions::to_string_t(this->consumer_key_);
     string_t oauth_nonce;
     string_t oauth_signature_method = U("HMAC-SHA1");
     string_t oauth_timestamp = utility::conversions::to_string_t(std::to_string(utc));
     string_t oauth_token = utility::conversions::to_string_t(this->oauth_access_token_key_);
     string_t oauth_version = U("1.0");
-
+    //=============this
     std::vector<unsigned char> seed;
     for (int i = 0; i < 32; ++i)
         seed.push_back(utc % (31 * (i % 7 + 1)));   //generates 32 bytes of pseudo random data
     oauth_nonce = utility::conversions::to_base64(seed); //might need to be stripped of "non-word" characters
 
-    // Decode base64 encoded key, secret.
-    string_t secret = U("+RDw7Q8V9EbHZusYzX3vfub0I80tytDs8RQPd3la8/wpKLShyf+B6113C3xxqiRy0r5c8UWiUmy+xSaATemWIg==");
-    std::vector<unsigned char> decoded_vec = utility::conversions::from_base64(secret);
-    std::cout << "Decoded secret has " << decoded_vec.size() << " bytes." << std::endl << std::endl;
-    std::cout << "Decoded secret is: ";
-    for (auto i : decoded_vec) {
-        std::cout << i;
-    }
-    std::cout << std::endl;
+    string_t combined_url = percentEncode(U("oauth_consumer_key=") + oauth_consumer_key)
+                            + percentEncode(U("&oauth_nonce=") + oauth_nonce)
+                            + percentEncode(U("&oauth_signature_method=") + oauth_signature_method)
+                            + percentEncode(U("&oauth_timestamp=") + oauth_timestamp)
+                            + percentEncode(U("&oauth_token=") + oauth_token)
+                            + percentEncode(U("&oauth_version=") + oauth_version);
+    
+
+    //// Decode base64 encoded key, secret.
+    //string_t secret = U("+RDw7Q8V9EbHZusYzX3vfub0I80tytDs8RQPd3la8/wpKLShyf+B6113C3xxqiRy0r5c8UWiUmy+xSaATemWIg==");
+    //std::vector<unsigned char> decoded_vec = utility::conversions::from_base64(secret);
+    //std::cout << "Decoded secret has " << decoded_vec.size() << " bytes." << std::endl << std::endl;
+    //std::cout << "Decoded secret is: ";
+    //for (auto i : decoded_vec) {
+    //    std::cout << i;
+    //}
 
     // Dump prehash into an arry of bytes.
    // unsigned char* ary_prehash = new unsigned char[prehash.size()];
-   /* for (size_t i = 0; i < prehash.size(); i++) {
-        ary_prehash[i] = static_cast<unsigned char>(prehash[i]);
-    }
-*/
-    // Dump decoded vector into an array of bytes.
-    unsigned char* ary_key = new unsigned char[decoded_vec.size()];
-    for (size_t i = 0; i < decoded_vec.size(); i++) {
-        ary_key[i] = static_cast<unsigned char>(decoded_vec[i]);
-    }
+//   /* for (size_t i = 0; i < prehash.size(); i++) {
+//        ary_prehash[i] = static_cast<unsigned char>(prehash[i]);
+//    }
+//*/
+//    // Dump decoded vector into an array of bytes.
+//    unsigned char* ary_key = new unsigned char[decoded_vec.size()];
+//    for (size_t i = 0; i < decoded_vec.size(); i++) {
+//        ary_key[i] = static_cast<unsigned char>(decoded_vec[i]);
+    //}
 
     // Generate signature using sha256 encryption on prehash with key ary_key.
     //unsigned char encrypted[EVP_MAX_MD_SIZE];
