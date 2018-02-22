5fac3bf3d7fe0b07c1cb6542a85760a05d2843a0
AndrewLaux <laux.m.andrew@gmail.com>
2018-02-21 20:24:44 -0800

    Now outputs to file.

--- src/watson.cpp
+++ src/watson.cpp
@@ -63,7 +63,7 @@ utility::string_t Watson::toneToString(const utility::string_t & tone_input) {
 }
 
 // Function: Outputs sentiment to <coinID>.josn.
-void Watson::toneRatingToFile(const utility::string_t & tone_input, const utility::string_t & filename) {
+web::json::value Watson::toneToJson(const utility::string_t & tone_input) {
 
 	// Response json.
 	web::json::value json_obj;
@@ -98,29 +98,94 @@ void Watson::toneRatingToFile(const utility::string_t & tone_input, const utilit
 
 	}).wait(); // Collect outstanding tasks.
 
+	// Return obj
+	return json_obj;	
+}
 
-	// Collect sentiment values.
-	auto value_tone = json_obj[U("document_tone")][U("tones")][0][U("tone_name")];
-	auto value_score = json_obj[U("document_tone")][U("tones")][0][U("score")];
-	double score = value_score.as_double();
-	utility::string_t tone = value_tone.as_string();
+// Function writes tone sentiment to file.
+void Watson::toneToFile(const utility::string_t & tone_input, const utility::string_t & file_name) {
+
+	// Call toneToJson to get json object.
+	web::json::value json_sentiment = toneToJson(tone_input);
+
+	// declare variables to track negative and positive ratings.
+	double positive_rating = 0;
+	double negative_rating = 0;
+	double count_neg = 0;
+	double count_pos = 0;
+
+	// Loop through tones given by json returned by watson.
+	for (size_t i = 0; i < json_sentiment[U("document_tone")][U("tones")].size(); i++) {
+
+		// Collect sentiment values.
+		auto value_tone = json_sentiment[U("document_tone")][U("tones")][i][U("tone_name")];
+		auto value_score = json_sentiment[U("document_tone")][U("tones")][i][U("score")];
+		double score = value_score.as_double();
+		utility::string_t tone = value_tone.as_string();
+
+		// Get rating.
+		double rating = score * rateTone(tone);
+
+		// Increment positive. 
+		if (rating > 0) {
+			positive_rating += rating;
+			count_pos++;
+		}
+
+		// Increment negative.
+		else {
+			negative_rating += rating;
+			count_neg++;
+		}
+
+	}
+
+	// Average ratings. 
+	positive_rating = positive_rating / count_pos;
+	negative_rating = -1*(negative_rating / count_neg);
+
+	// TODO: Throw logical error if ratings are < 0.5 or >1 
 
 	// Get timestamp.
 	int utc = static_cast<int>(this->date.utc_timestamp());
 
-	// Get index. Multiply score by rating.
-	double rating = score * rateTone(tone);
-
 	// Open outgoing file stream.
-	utility::ofstream_t outfile;
-	outfile.open(filename);
+	std::ifstream check_file(file_name);
 
-	// Check that stream is open
-	if (outfile.fail()) {
-		//TODO: throw exception
+	// Create json doc
+	rapidjson::Document doc;
+
+	// File already exists.
+	if (check_file.good()) {
+
+		// Wrap std::ifstream object.
+		rapidjson::IStreamWrapper isw(check_file);
+		doc.ParseStream(isw);
 	}
-	
+
+	// File doesn't exist.
+	else doc.SetArray();
+
+	//Allocator
+	rapidjson::Document::AllocatorType& allocator = doc.GetAllocator();
+
+	// Generate entry.
+	rapidjson::Value v;
+	v.SetObject();
+	v.AddMember("PosRating", rapidjson::Value(positive_rating), allocator);
+	v.AddMember("NegRating", rapidjson::Value(negative_rating), allocator);
+	v.AddMember("Time", rapidjson::Value(utc), allocator);
+	doc.PushBack(v, allocator);
+
+	// Use json writer to output to file.
+	std::ofstream out(file_name);
+	rapidjson::OStreamWrapper osw(out);
+	rapidjson::Writer<rapidjson::OStreamWrapper> writer(osw);
+	doc.Accept(writer);
+	out.close();
+
 }
+
 // Helper function rates tone. 1 for pos, 0 for neutral, -1 for neg.
 int Watson::rateTone(const utility::string_t & tone) {
 
@@ -131,7 +196,7 @@ int Watson::rateTone(const utility::string_t & tone) {
 	if (tone == U("Satisfied")) rating = 1;
 	else if (tone == U("Excited")) rating = 1;
 	else if (tone == U("Joy")) rating = 1;
-	else if (tone == U("")) rating = 1;
+	else if (tone == U("Confident")) rating = 1;
 
 	// Neutral tones.
 	else if (tone == U("Polite")) rating = 0;
--- src/watson_test.cpp
+++ src/watson_test.cpp
@@ -16,12 +16,13 @@ using namespace concurrency::streams;       // Asynchronous streams
 int main() {
 
 	// Sentiment to analyze.
-	string_t comment = U("We don't serve droids here.\nI love you.\n This is gonna be good!");
+	string_t comment = U("We don't serve droids here.\nI love you.\n This is gonna be good!\n Just the tip, baby. \n Obama is a nagger. \n You smell like eggs. \n I'm looking forward to killing myself. \n Panda express tasted really good today. \n My mother won't stop calling me. \n When I fart it smells like toothpaste. \n");
 
-	//Watson obj.
+	// Watson obj.
 	Watson watson_bot(U("c032fda0-5c02-490d-8e00-ab00de2e5f40"), U("AfgP2LQIDCgB"));
 
-
+	// Check.
+	watson_bot.toneToFile(comment, U("c:/users/Andrew Laux/Documents/ApolloTradingBot/resources/test.json"));
 	
 
 	// Exit program.
